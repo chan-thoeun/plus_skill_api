2d491fb8b72e00e13a430ce801da7fdd4797b4b1
chreounsary <chreounsary@gmail.com>
2023-04-01 12:01:05 +0700

    connect wallet

--- resources/js/src/app.js
+++ resources/js/src/app.js
@@ -9,14 +9,19 @@ import { store } from '../src/redux/store';
 import Theme from '../src/components/common/theme';
 import { MouseMoveProvider } from '../src/contexts/mouse-move-context';
 import MyApp from '../src/pages/_app';
+import { Mumbai } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
 	<React.Fragment>
 	<Provider store={ store }>
 		<ThemeProvider defaultTheme="light">
 			<MouseMoveProvider>
-				<MyApp />
+				<ThirdwebProvider supportedChains={[Mumbai]}>
+					<MyApp />
+				</ThirdwebProvider>
 			</MouseMoveProvider>
 			<Theme />
 		</ThemeProvider>
